54a9e88a55dbc4c5669d271f73adc91688b2a822
Theo Hagry <t.hagry@student.isartdigital.com>
2020-02-18 15:55:51 +0100

    [Add] Interfaces for component base and updatable
    Still requires to put the components into lists to use the update

---
+++ Engine/Engine/include/Core/Datastructure/IComponent.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+namespace Core::Datastructure
+{
+	class Object;
+
+	class IComponent
+	{
+	private:
+		bool		m_isDestroyed = false;
+		bool		m_isInit = false;
+	protected:
+		bool		m_isActive = false;
+		Object* m_parent{ nullptr };
+	public:
+		void			SetParent(Object* parent)
+		{
+			m_parent = parent;
+		}
+
+		/**
+		 * Function called by the engine to start a component.
+		 */
+		inline void		Start();
+
+		/**
+		 * Marks the component for destruction, calling OnDestroy immediatly
+		 * and the destructor afterward
+		 */
+		inline void		Destroy();
+
+		/**
+		 * Returns if the component has been destroyed
+		 * @return State of the component
+		 */
+		bool			IsDestroyed() const noexcept { return m_isDestroyed; }
+		/**
+		 * Returns if the component was initialized
+		 * @return State of the component
+		 */
+		bool			IsInit() const noexcept { return m_isInit; };
+		/**
+		 * Returns if the component is active
+		 * @return State of the component
+		 */
+		bool			IsActive() const noexcept { return m_isActive; }
+
+		/**
+		 * Called on start of the component, on the first frame it
+		 * is active
+		 */
+		virtual void	OnStart() = 0;
+		/**
+		 * Called when destruction is asked. Destructor will be called
+		 * at the end of the frame
+		 */
+		virtual void	OnDestroy() = 0;
+	};
+
+
+	void IComponent::Start()
+	{
+		if (m_isInit || !m_isActive)
+			return;
+		OnStart();
+		m_isInit = true;
+	}
+
+	void IComponent::Destroy()
+	{
+		if (m_isDestroyed)
+			return;
+		OnDestroy();
+		m_isDestroyed = true;
+	}
+}
