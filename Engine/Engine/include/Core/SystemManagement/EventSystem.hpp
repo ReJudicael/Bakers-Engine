e1bfd259bb8e77e082d6c75d305812d01d2226f8
Judicaël <charlotjudicael@outlook.fr>
2020-02-20 10:52:59 +0100

    [Add] GetLastListenerID added, rename m_availableListenerID, add comments

--- Engine/Engine/include/Core/SystemManagement/EventSystem.hpp
+++ Engine/Engine/include/Core/SystemManagement/EventSystem.hpp
@@ -3,107 +3,150 @@
 #include <unordered_map>
 #include <functional>
 
-
-
-namespace SystemManagement
+namespace Core
 {
-    typedef unsigned int ID;
+	/**
+	 * Contains all system tools of the engine
+	 */
+	namespace SystemManagement
+	{
+		typedef unsigned int ID;
 
-    /**
-     * Handle callback hierarchy and event system
-     * @tparam Args: Params of method sent
-     */
-    template<class... Args>
-    class EventSystem final
-    {
-    private:
-        using Event = std::function<void(Args...)>;
+		/**
+		 * Handle callback hierarchy and event system
+		 * @tparam Args: Params of method sent
+		 */
+		template<class... Args>
+		class EventSystem final
+		{
+		private:
+			using Event = std::function<void(Args...)>;
 
-        /**
-         * Each Listener has its Event
-         * @T1 ID: Listener's ID to identify its callback
-         * @T2 Event: Function associated to Event
-         */
-        std::unordered_map<ID, Event> m_listenerEventMap;
-        ID m_availableListenerID{ 0 };
+			/**
+			 * Each Listener has its Event
+			 * @T1 ID: Listener's ID to identify its callback
+			 * @T2 Event: Function associated to Event
+			 */
+			std::unordered_map<ID, Event> m_listenerEventMap;
 
-    public:
-        /**
-         * Add a listener to the event
-         * @param call: Callback function
-         * @return The ID of created listener
-         */
-        ID operator+=(const Event& call) noexcept;
+			/**
+			 * Available ID to create a new Listener
+			 */
+			ID m_availableIDForNewListener{ 1 };
 
-        /**
-         * Add a listener to the event
-         * @param call: Callback function
-         * @return The ID of created listener
-         */
-        ID AddListener(const Event& call) noexcept;
+		public:
+			/**
+			 * Call each stored functions attached to its event
+			 * @params args: Arguments for the call (Variadic arguments)
+			 */
+			void operator()(Args... args) noexcept;
 
-        /**
-         * Remove a listener through its ID
-         * @param id: ID of listener to remove
-         * @return Whether the deletion work or not
-         */
-        bool RemoveListener(ID listenerID) noexcept;
+			/**
+			 * Add a listener to the event
+			 * @param call: Callback function
+			 * @return The ID of created listener
+			 */
+			ID operator+=(const Event& call) noexcept;
 
-        /**
-         * Remove all listeners
-         */
-        void RemoveAllListeners() noexcept;
+			/**
+			 * Remove a listener through its ID
+			 * @param listenerID: ID of listener to remove
+			 * @return Whether the deletion work or not
+			 */
+			bool operator-=(ID listenerID) noexcept;
 
-        /**
-         * Call each stored functions attached to its event
-         * @params args: Arguments for the call (Variadic arguments)
-         */
-        void Invoke(Args... args) noexcept;
+			/**
+			 * Add a listener to the event
+			 * @param call: Callback function
+			 * @return The ID of created listener
+			 */
+			ID AddListener(const Event& call) noexcept;
 
-        /**
-         * Get the number of event registered
-         * @return Number of event registered
-         */
-        size_t GetEventCount() const noexcept;
-    };
+			/**
+			 * Remove a listener through its ID
+			 * @param listenerID: ID of listener to remove
+			 * @return Whether the deletion work or not
+			 */
+			bool RemoveListener(ID listenerID) noexcept;
 
-    template<class... Args>
-    inline ID EventSystem<Args...>::operator+=(const Event& call) noexcept
-    {
-        return AddListener(call);
-    }
+			/**
+			 * Remove all listeners
+			 */
+			void RemoveAllListeners() noexcept;
 
-    template<class... Args>
-    inline ID EventSystem<Args...>::AddListener(const Event& call) noexcept
-    {
-        ID listenerID = m_availableListenerID;
-        m_listenerEventMap.emplace(listenerID, call);
-        m_availableListenerID++;
-        return listenerID;
-    }
+			/**
+			 * Call each stored functions attached to its event
+			 * @params args: Arguments for the call (Variadic arguments)
+			 */
+			void Invoke(Args... args) noexcept;
 
-    template<class... Args>
-    inline bool EventSystem<Args...>::RemoveListener(ID listenerID) noexcept
-    {
-        return m_listenerEventMap.erase(listenerID) != 0;
-    }
+			/**
+			 * Get the number of event registered
+			 * @return Number of event registered
+			 */
+			size_t GetEventCount() const noexcept;
 
-    template<class... Args>
-    inline void EventSystem<Args...>::RemoveAllListeners() noexcept
-    {
-        m_listenerEventMap.clear();
-    }
+			/**
+			 * Get ID of last listener
+			 * @return ID of last listener or 0 if there is no listener
+			 */
+			ID GetLastListenerID() const noexcept;
+		};
 
-    template<class... Args>
-    inline void EventSystem<Args...>::Invoke(Args... args) noexcept
-    {
-        for (const auto& [Key, T] : m_listenerEventMap)
-            T(args...);
-    }
+		template<class... Args>
+		inline void EventSystem<Args...>::operator()(Args... args) noexcept
+		{
+			Invoke(args...);
+		}
 
-    template<class ...Args>
-    inline size_t EventSystem<Args...>::GetEventCount() const noexcept
-    {
-        return m_listenerEventMap.size();
-    }
+		template<class... Args>
+		inline ID EventSystem<Args...>::operator+=(const Event& call) noexcept
+		{
+			return AddListener(call);
+		}
+
+		template<class... Args>
+		inline bool EventSystem<Args...>::operator-=(ID listenerID) noexcept
+		{
+			return RemoveListener(listenerID);
+		}
+
+		template<class... Args>
+		inline ID EventSystem<Args...>::AddListener(const Event& call) noexcept
+		{
+			m_listenerEventMap.emplace(m_availableIDForNewListener, call);
+			return m_availableIDForNewListener++;
+		}
+
+		template<class... Args>
+		inline bool EventSystem<Args...>::RemoveListener(ID listenerID) noexcept
+		{
+			return m_listenerEventMap.erase(listenerID) != 0;
+		}
+
+		template<class... Args>
+		inline void EventSystem<Args...>::RemoveAllListeners() noexcept
+		{
+			m_listenerEventMap.clear();
+		}
+
+		template<class... Args>
+		inline void EventSystem<Args...>::Invoke(Args... args) noexcept
+		{
+			for (const auto& [Key, T] : m_listenerEventMap)
+				T(args...);
+		}
+
+		template<class ...Args>
+		inline size_t EventSystem<Args...>::GetEventCount() const noexcept
+		{
+			return m_listenerEventMap.size();
+		}
+
+		template<class ...Args>
+		inline ID EventSystem<Args...>::GetLastListenerID() const noexcept
+		{
+			return m_availableIDForNewListener - 1;
+		}
+	}
 }
