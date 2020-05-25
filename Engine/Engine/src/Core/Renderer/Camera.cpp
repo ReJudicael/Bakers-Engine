8c3801c86693717d927f895eb646a4c1b770aa0d
Theo Hagry <t.hagry@student.isartdigital.com>
2020-05-25 14:42:53 +0200

    [Fix] Fixed camera

--- Engine/Engine/src/Core/Renderer/Camera.cpp
+++ Engine/Engine/src/Core/Renderer/Camera.cpp
@@ -38,7 +38,7 @@ namespace Core::Renderer
 		return CreatePerspectiveMatrix(m_persp.width, m_persp.height, m_persp.fov, m_persp.near, m_persp.far);
 	}
 
-	Camera::Camera() : ComponentBase(), ICamera(), IUpdatable()
+	Camera::Camera()
 	{
 	}
 
