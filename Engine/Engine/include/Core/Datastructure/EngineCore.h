ab2436b906d27089a0d03c8fcecb03c8501de2b4
Theo Hagry <t.hagry@student.isartdigital.com>
2020-06-03 08:13:52 +0200

    [Add] Comments

--- Engine/Engine/include/Core/Datastructure/EngineCore.h
+++ Engine/Engine/include/Core/Datastructure/EngineCore.h
@@ -37,7 +37,10 @@ namespace Core
 		
 		static const char* glsl_version = "#version 400";
 		class RootObject;
-
+		
+		/**
+		 * Core of the engine. Contains everything necessary to run the engine
+		 */
 		BAKERS_API_CLASS EngineCore
 		{
 		protected:
@@ -70,6 +73,9 @@ namespace Core
 			Core::SystemManagement::EventSystem<int, int> OnResizeWindow;
 
 		protected:
+			/**
+			 * Returns the computed deltaTime of the frame. To call once per frame internally
+			 */
 			double GetDeltaTime();
 
 		public:
@@ -90,10 +96,22 @@ namespace Core
 			 */
 			virtual ~EngineCore();
 
+			/**
+			 * Sets the size of the window internally. Will not actually change the size of the window
+			 */
+			virtual void		SetSizeWindow(const int width, const int height) = 0;
 		public:
 
 
+			/**
+			 * Calls Init with default width and height as parameters
+			 */
 			int	Init();
+
+			/**
+			 * Initializes engine with given width and height for the window. 
+			 * Initializes opengl and loads default scene
+			 */
 			int	Init(const int width, const int height);
 			
 			/**
@@ -119,23 +137,62 @@ namespace Core
 			 */
 			void CreateSkybox(bool cube = true);
 
-			virtual void		SetSizeWindow(const int width, const int height) = 0;
+			/**
+			 * Calls all the function that must be called at the start of a frame
+			 */
 			virtual void		StartFrame();
+			/**
+			 * Calls all the function that must be called to render the engine
+			 */
 			virtual void		Render();
+			/**
+			 * Calls all the function that must be called at the end of a frame
+			 */
 			virtual void		EndFrame();
 
+			/**
+			 * Calls all functions necessary to update a frame of the engine
+			 * @param deltaTime: detaTime of the update
+			 */
 			void				Update(double deltaTime); 
 
 			Object*				SearchObjectInScene(const Core::Maths::Vec3& origin, const Core::Maths::Vec3& dir);
+
+			/**
+			 * Loads the scene given as path
+			 * @param scene: Scene to load
+			 */
 			bool				LoadScene(const std::string& scene);
+			/**
+			 * Gets the name of the current scene
+			 */
 			const std::string&	GetSceneName() const { return m_currScene; }
 
 		protected:
+			/**
+			 * Called in Init(), must be overriden to implement window
+			 */
 			virtual int			OnInit(const int width, const int height);
+			/**
+			 * Called for each frame
+			 */
 			virtual void		OnLoop();
+			/**
+			 * Called in StartFrame. Must be overriden to call all user code that needs to be called at the start of a frame
+			 */
 			virtual void		OnStartFrame();
+			/**
+			 * Called in OnUpdate. Must be overriden to call all user code that needs to be called in an update of a frame
+			 */
 			virtual void		OnUpdate(double deltaTime);
+			/**
+			 * Loads the current scene. If the name has not changed, reloads the scene
+			 */
 			bool				OnLoadScene();
+			/**
+			 * Extracts scene data from given json scene and loads it in the scene
+			 * @param scene: Json scene to be loaded
+			 */
 			void				LoadSceneFromJson(json& scene);
 
 		public:
@@ -159,10 +216,31 @@ namespace Core
 			Core::SystemManagement::InputSystem*	GetInputSystem();
 			Core::Audio::AudioSystem*				GetAudioSystem();
 
+			/**
+			 * Returns the number of fbo object of the given type
+			 */
 			int								GetFBONum(Core::Renderer::FBOType t);
+			/**
+			 * Retuns the fbo object of the given type and num
+			 * @param num: Number of the fbo object
+			 * @param t: Type of the fbo object
+			 * @return requested fbo object or nullptr if it does not exists
+			 */
 			Core::Renderer::Framebuffer*	GetFBO(int num, Core::Renderer::FBOType t = Core::Renderer::FBOType::CUSTOM);
+			/**
+			 * Creates default fbo object of current window size and type custom
+			 */
 			Core::Renderer::Framebuffer*	CreateFBO();
+			/**
+			 * Creates a fbo object of given size and given type
+			 * @param width: width of the fbo
+			 * @param width: height of the fbo
+			 * @param t: type of the fbo
+			 */
 			Core::Renderer::Framebuffer*	CreateFBO(int width, int height, Core::Renderer::FBOType t = Core::Renderer::FBOType::CUSTOM);
+			/**
+			 * Deletes the given FBO object
+			 */
 			void							DeleteFBO(Core::Renderer::Framebuffer* fbo);
 
 			virtual Core::Maths::Vec2		GetMousePos() noexcept = 0;
